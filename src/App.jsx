c971834a80868613bb2b2c4901e642fde0f631ba
YL Ang <skya.ayl@gmail.com>
2025-07-22 22:06:04 +0800

    Setup project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import ",/App.css";
+import IngredientList from "./components/IngredientList/IngredientList";
+import BurgerStack from "./components/BurgerStack/BurgerStack";
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  const availableIngredients = [
+    { name: "Kaiser Bun", color: "saddlebrown" },
+    { name: "Sesame Bun", color: "sandybrown" },
+    { name: "Gluten Free Bun", color: "peru" },
+    { name: "Lettuce Wrap", color: "olivedrab" },
+    { name: "Beef Patty", color: "#3F250B" },
+    { name: "Soy Patty", color: "#3F250B" },
+    { name: "Black Bean Patty", color: "#3F250B" },
+    { name: "Chicken Patty", color: "burlywood" },
+    { name: "Lettuce", color: "lawngreen" },
+    { name: "Tomato", color: "tomato" },
+    { name: "Bacon", color: "maroon" },
+    { name: "Onion", color: "lightyellow" },
+    { name: "Cheddar Cheese", color: "#FDE18B" },
+    { name: "Swiss Cheese", color: "#F1E1A8" },
+  ];
+
+  const [stack, setStack] = useState([]);
+
+  return (
+    <main>
+      <h1>Burger Stacker</h1>
+      <section>
+        <IngredientList />
+        <BurgerStack />
+      </section>
+    </main>
+  );
+};
 
 export default App;
---
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -0,0 +1,5 @@
+const BurgerStack = (props) => {
+  return <ul>// map through props.ingredients</ul>;
+};
+
+export default BurgerStack;
